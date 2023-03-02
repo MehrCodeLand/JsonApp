c24f31b09b4d2d124f02135cdc9002a9d4f3077e
Mehrshad <lastenigmas@gmail.com>
2023-03-03 01:16:00 +0330

    Add project files.

---
+++ JsonApp/MainApp.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+
+// first install Newtonsoft.Json !
+namespace JsonApp
+{
+    public class MainApp
+    {
+        public static void Main()
+        {
+            CreateJsonFile();
+            
+            WriteData();
+        }
+
+        public static void CreateJsonFile()
+        {
+            if (!File.Exists(@"C:\JsonAppFile\JsonFile.json"))
+            {
+                System.IO.Directory.CreateDirectory(@"C:\JsonAppFile");
+                var myFile = File.Create(@"C:\JsonAppFile\JsonFile.json");
+                myFile.Close();
+            }
+        }
+        public static List<User> CreateData()
+        {
+            List<User> users = new List<User>();
+            User user1 = new User()
+            {
+                Username = "mehrshad",
+                Password = "1234",
+            };
+
+            User user2 = new User()
+            {
+                Username = "Aria",
+                Password = "4321",
+            };
+
+            users.Add(user1);
+            users.Add(user2);
+
+            return users;
+        }
+        public static string CreateJsonString()
+        {
+            var jsonStr = JsonConvert.SerializeObject(CreateData());
+            return jsonStr;
+        }
+        public static void WriteData()
+        {
+            var jsonStr = CreateJsonString();
+            var path = @"C:\JsonAppFile\JsonFile.json";
+            File.WriteAllText(path, jsonStr);
+        }
+    }
+
+    public class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
